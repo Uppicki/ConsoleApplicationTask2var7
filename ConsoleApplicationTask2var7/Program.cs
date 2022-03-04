3c7c1dc4070e3547d8c5b3cdcf26227e3fa4d8c4
Your Name <vladimiruppicki@gmail.com>
2022-03-04 13:48:19 +0300

    Initial commit

---
+++ ConsoleApplicationTask2var7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApplicationTask2var7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"P:\RiderProjects\ConsoleApplicationTask2var7\";
+            string pathIn = path + @"ConsoleApplicationTask2var7\input.txt";
+            Console.WriteLine("Write output file name");
+            string pathOut = path + Console.ReadLine() + "out.txt";
+
+            //string[] input = readFromFile(pathIn);
+            
+            string[] lines = readFromFile(pathIn);
+            
+            writeToFile(pathOut, lines);
+        }
+
+        private static string[] readFromFile(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+        
+        private static void writeToFile(string path, string[] text)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(path, false);
+                foreach (string line in text)
+                {
+                    sw.Write(line + " ");
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
